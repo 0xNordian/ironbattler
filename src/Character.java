df477644f76e1e03924367f9b79cc1cc28868c5e
Marina M <marnacode@gmail.com>
2023-01-15 18:31:08 +0100

    setup commit

---
+++ src/Character.java
@@ -0,0 +1,45 @@
+public abstract class Character {
+
+    private String id;
+    private String name;
+    private int hp;
+    private boolean isAlive = true;
+
+    public Character(String id, String name, int hp, boolean isAlive) {
+        this.id = id;
+        setName(name);
+        setHp(hp);
+        this.isAlive = isAlive;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void setAlive(boolean alive) {
+        isAlive = alive;
+    }
+}
