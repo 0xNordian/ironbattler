0e33658e281611555f4a46b90feec2cdb317ef87
0xNordian <85548493+0xNordian@users.noreply.github.com>
2023-01-17 22:01:40 +0100

    menu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+/*
 
         Warrior w1 = new Warrior("NT");
         w1.attack();
@@ -24,5 +27,48 @@ public class Main {
                         "Strength: " + w2.getStrength() + '\n' +
                         "Stamina: " + w2.getStamina() + '\n'
         );
+ */
+
+        Scanner sc = new Scanner(System.in);
+        int choice;
+        do {
+            System.out.println("Select game mode:");
+            System.out.println("1) Create characters");
+            System.out.println("2) Create random characters");
+            System.out.println("3) Exit");
+            choice = sc.nextInt();
+            sc.nextLine();
+            if (choice == 1) {
+                int count = 0;
+                while (count < 2) {
+                    System.out.println("Create characters:");
+                    System.out.println("1) Warrior");
+                    System.out.println("2) Wizard");
+                    int subChoice = sc.nextInt();
+                    sc.nextLine();
+                    if (subChoice == 1) {
+                        System.out.println("Enter name for warrior:");
+                        String name = sc.nextLine();
+                        System.out.println("Enter hp for warrior between 100-200:");
+                        int hp = sc.nextInt();
+                        Warrior warrior = new Warrior(name, hp);
+                        System.out.println("Warrior created: " + "\n" + "Name: " + warrior.getName() + "\n"  + "Hp: " + warrior.getHp() + "hp " + "\n"   + "Strength: " + warrior.getStrength() + "\n" + "Stamina: " + warrior.getStamina());
+                    } else if (subChoice == 2) {
+                        System.out.println("Enter name for wizard:");
+                        String name = sc.nextLine();
+                        System.out.println("Enter hp for wizard between 50-100:");
+                        int hp = sc.nextInt();
+                        Wizard wizard = new Wizard(name, hp);
+                        System.out.println("Wizard created: " + "\n" + "Name: " + wizard.getName() + "\n"  + "Hp: " + wizard.getHp() + "hp " + "\n"   + "Intelligence: " + wizard.getIntelligence() + "\n" + "Mana: " + wizard.getMana());
+                    }
+                    count++;
+                }
+                System.out.println("Let the battle begin!");
+                break;
+            } else if (choice == 2) {
+                // code random characters
+            }
+        } while (choice != 3);
+
     }
 }
