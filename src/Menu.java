01e6b653b7c0d0864af8415f39de21411604e383
0xNordian <85548493+0xNordian@users.noreply.github.com>
2023-01-19 09:44:40 +0100

    menu completed

--- src/Menu.java
+++ src/Menu.java
@@ -1,9 +1,11 @@
+
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Menu {
     public static void startMenu() {
+        ArrayList<Character> characters = new ArrayList<Character>();
         String art = """
 
                 ██ ██████   ██████  ███    ██ ██████   █████  ████████ ████████ ██      ███████\s
@@ -30,47 +32,80 @@ public class Menu {
                 continue;
             }
             if (choice == 1) {
+                createCharacter();
+                System.out.println("Let the battle begin! First");
+
+                break;
+            } else if (choice == 2) {
                 int count = 0;
                 while (count < 2) {
-                    System.out.println("⏜⏝⏜⏝⏜⏝⏜⏝⏜⏝⏜⏝⏜⏝");
-                    System.out.println("Create characters:");
-                    System.out.println("1) Warrior ⚔️\uD83D\uDEE1️");
-                    System.out.println("2) Wizard \uD83E\uDDD9\u200D️\uD83D\uDD2E");
-                    int subChoice = sc.nextInt();
-                    sc.nextLine();
-                    if (subChoice < 1 || subChoice > 2) {
-                        System.out.println("Invalid option, please select 1 or 2");
-                        continue;
-                    }
-                    if (subChoice == 1) {
-                        String artWarrior = """
-                                   |\\                     /)
-                                 /\\_\\\\__               (_//
-                                |   `>\\-`     _._       //`)
-                                 \\ /` \\\\  _.-`:::`-._  //
-                                  `    \\|`    :::    `|/
-                                        |     :::     |
-                                        |.....:::.....|
-                                        |:::::::::::::|
-                                        |     :::     |
-                                        \\     :::     /
-                                         \\    :::    /
-                                          `-. ::: .-'
-                                           //`:::`\\\\
-                                          //   '   \\\\
-                                         |/         \\\\      
-                                """;
+                    createRandomCharacter();
+                    count++;
+                }
+                System.out.println("Let the battle begin!");
 
-                        artWarrior.lines().forEach(System.out::println);
-                        System.out.println("Enter name for warrior:");
-                        String name = sc.nextLine();
-                        System.out.println("Enter hp for warrior between 100-200:");
-                        int hp = sc.nextInt();
-                        Warrior warrior = new Warrior(name, hp);
-                        System.out.println("⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄");
-                        System.out.println("⚔️\uD83D\uDEE1 Warrior created: " + "\n" + "⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄" + "\n" + "Name: " + warrior.getName() + "\n" + "❤️Hp: " + warrior.getHp() + "hp " + "\n" + "💪Strength: " + warrior.getStrength() + "\n" + "🦵Stamina: " + warrior.getStamina());
-                    } else if (subChoice == 2) {
-                        String artWarrior = """
+                break;
+            }
+        }
+        while (choice != 3);
+    }
+
+    private static void createCharacter(){
+        Scanner sc = new Scanner(System.in);
+        int count = 0;
+        while (count < 2) {
+            System.out.println("⏜⏝⏜⏝⏜⏝⏜⏝⏜⏝⏜⏝⏜⏝");
+            System.out.println("Create characters:");
+            System.out.println("1) Warrior ⚔️\uD83D\uDEE1️");
+            System.out.println("2) Wizard \uD83E\uDDD9\u200D️\uD83D\uDD2E");
+            int subChoice = sc.nextInt();
+            sc.nextLine();
+            if (subChoice < 1 || subChoice > 2) {
+                System.out.println("Invalid option, please select 1 or 2");
+                continue;
+            }
+            if (subChoice == 1) {
+                warriorCharacterGenerator();
+            } else if (subChoice == 2) {
+                wizardCharacterGenerator();
+            }
+            count++;
+        }
+    }
+
+    private static void warriorCharacterGenerator(){
+        Scanner sc = new Scanner(System.in);
+        String artWarrior = """
+                                       |\\                     /)
+                                     /\\_\\\\__               (_//
+                                    |   `>\\-`     _._       //`)
+                                     \\ /` \\\\  _.-`:::`-._  //
+                                      `    \\|`    :::    `|/
+                                            |     :::     |
+                                            |.....:::.....|
+                                            |:::::::::::::|
+                                            |     :::     |
+                                            \\     :::     /
+                                             \\    :::    /
+                                              `-. ::: .-'
+                                               //`:::`\\\\
+                                              //   '   \\\\
+                                             |/         \\\\      
+                                    """;
+
+        artWarrior.lines().forEach(System.out::println);
+        System.out.println("Enter name for warrior:");
+        String name = sc.nextLine();
+        System.out.println("Enter hp for warrior between 100-200:");
+        int hp = sc.nextInt();
+        Warrior warrior = new Warrior(name, hp);
+        System.out.println("⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄");
+        System.out.println("⚔️\uD83D\uDEE1 Warrior created: " + "\n" + "⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄" + "\n" + "Name: " + warrior.getName() + "\n" + "❤️Hp: " + warrior.getHp() + "hp " + "\n" + "💪Strength: " + warrior.getStrength() + "\n" + "🦵Stamina: " + warrior.getStamina());
+    }
+
+    private static void wizardCharacterGenerator(){
+        Scanner sc = new Scanner(System.in);
+        String artWarrior = """
                                             *    .
                                      '  +   ___    @    .
                                          .-" __"-.   +
@@ -83,31 +118,15 @@ public class Menu {
                                   *     `-.,___,.-'     
                                 """;
 
-                        artWarrior.lines().forEach(System.out::println);
-                        System.out.println("Enter name for wizard:");
-                        String name = sc.nextLine();
-                        System.out.println("Enter hp for wizard between 50-100:");
-                        int hp = sc.nextInt();
-                        Wizard wizard = new Wizard(name, hp);
-                        System.out.println("⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄");
-                        System.out.println("\uD83E\uDDD9\u200D️\uD83D\uDD2E Wizard created: " + "\n" + "⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄" + "\n" + "Name: " + wizard.getName() + "\n" + "❤️Hp: " + wizard.getHp() + "hp " + "\n" + "🧠Intelligence: " + wizard.getIntelligence() + "\n" + "💠Mana: " + wizard.getMana());
-                    }
-                    count++;
-                }
-                System.out.println("Let the battle begin!");
-                break;
-            } else if (choice == 2) {
-                int count = 0;
-                while (count < 2) {
-                    createRandomCharacter();
-                    count++;
-                }
-                System.out.println("Let the battle begin!");
-                break;
-            }
-        } while (choice != 3);
+        artWarrior.lines().forEach(System.out::println);
+        System.out.println("Enter name for wizard:");
+        String name = sc.nextLine();
+        System.out.println("Enter hp for wizard between 50-100:");
+        int hp = sc.nextInt();
+        Wizard wizard = new Wizard(name, hp);
+        System.out.println("⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄");
+        System.out.println("\uD83E\uDDD9\u200D️\uD83D\uDD2E Wizard created: " + "\n" + "⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄" + "\n" + "Name: " + wizard.getName() + "\n" + "❤️Hp: " + wizard.getHp() + "hp " + "\n" + "🧠Intelligence: " + wizard.getIntelligence() + "\n" + "💠Mana: " + wizard.getMana());
     }
-
 
     public static void createRandomCharacter() {
         int randomNum = (int) (Math.random() * 2) + 1;
@@ -121,41 +140,40 @@ public class Menu {
             System.out.println("\uD83E\uDDD9\u200D️\uD83D\uDD2E Wizard created: " + "\n" + "⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄⌃⌄" + "\n" + "Name: " + wizard.getName() + "\n" + "❤️Hp: " + wizard.getHp() + "hp " + "\n" + "🧠Intelligence: " + wizard.getIntelligence() + "\n" + "💠Mana: " + wizard.getMana());
         }
     }
-        private static ArrayList<String> fantasyNames = new ArrayList<>();
+    private static ArrayList<String> fantasyNames = new ArrayList<>();
 
-        static {
-            fantasyNames.add("Gimli");
-            fantasyNames.add("Legolas");
-            fantasyNames.add("Aragorn");
-            fantasyNames.add("Boromir");
-            fantasyNames.add("Samwise");
-            fantasyNames.add("Thorin");
-            fantasyNames.add("Gollum");
-            fantasyNames.add("Bilbo");
-            fantasyNames.add("Eowyn");
-            fantasyNames.add("Galadriel");
-            fantasyNames.add("Arwen");
-            fantasyNames.add("Elrond");
-            fantasyNames.add("Gandalf");
-            fantasyNames.add("Frodo");
-            fantasyNames.add("Sauron");
-            fantasyNames.add("Merlin");
-            fantasyNames.add("Alfred");
-            fantasyNames.add("Dumbledore");
-            fantasyNames.add("Harry");
-            fantasyNames.add("Hermione");
-            fantasyNames.add("Ron");
-            fantasyNames.add("Voldemort");
-            fantasyNames.add("Ginny");
-            fantasyNames.add("Draco");
-            fantasyNames.add("Snape");
-        }
-
-        public static String generateRandomName() {
-            Random rand = new Random();
-            int randomIndex = rand.nextInt(fantasyNames.size());
-            return fantasyNames.get(randomIndex);
-        }
+    static {
+        fantasyNames.add("Gimli");
+        fantasyNames.add("Legolas");
+        fantasyNames.add("Aragorn");
+        fantasyNames.add("Boromir");
+        fantasyNames.add("Samwise");
+        fantasyNames.add("Thorin");
+        fantasyNames.add("Gollum");
+        fantasyNames.add("Bilbo");
+        fantasyNames.add("Eowyn");
+        fantasyNames.add("Galadriel");
+        fantasyNames.add("Arwen");
+        fantasyNames.add("Elrond");
+        fantasyNames.add("Gandalf");
+        fantasyNames.add("Frodo");
+        fantasyNames.add("Sauron");
+        fantasyNames.add("Merlin");
+        fantasyNames.add("Alfred");
+        fantasyNames.add("Dumbledore");
+        fantasyNames.add("Harry");
+        fantasyNames.add("Hermione");
+        fantasyNames.add("Ron");
+        fantasyNames.add("Voldemort");
+        fantasyNames.add("Ginny");
+        fantasyNames.add("Draco");
+        fantasyNames.add("Snape");
     }
 
+    public static String generateRandomName() {
+        Random rand = new Random();
+        int randomIndex = rand.nextInt(fantasyNames.size());
+        return fantasyNames.get(randomIndex);
+    }
 
+}
